85a9eb617a9cd432f5e8fbdf4b4b1735ffb6e8e0
Wendy Fu <wendy@wovn.io>
2021-12-30 11:01:49 +0700

    Introducing vue-router

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,19 +1,43 @@
 <template>
-  <div class="hello">
-    <h1>Welcome to the Forum</h1>
-    <div v-for="thread in threads" :key="thread['.key']">
-      <h2>{{ thread.title }}</h2>
-      <div v-for="postId in thread.posts" :key="postId">
-        <p>{{users[posts[postId].userId].name}}</p>
-        <p>{{posts[postId].text}}</p>
+  <div>
+    <div v-for="thread in threads"
+      :key="thread['.key']"
+      class="col-large push-top"
+    >
+      <h1>{{thread.title}}</h1>
+
+      <div class="post-list">
+        <div v-for="postId in thread.posts"
+          :key="postId"
+          class="post"
+        >
+          <div class="user-info">
+            <a href="#" class="user-name">{{users[posts[postId].userId].name}}</a>
+
+            <a href="#">
+              <img class="avatar-large" :src="users[posts[postId].userId].avatar" alt="">
+            </a>
+
+            <p class="desktop-only text-small">107 posts</p>
+          </div>
+
+          <div class="post-content">
+            <div>
+              {{posts[postId].text}}
+            </div>
+          </div>
+
+          <div class="post-date text-faded">
+            {{posts[postId].publishedAt}}
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import sourceData from '@/data.json'
-console.log(sourceData)
+import sourceData from '@/data'
 
 export default {
   name: 'HelloWorld',
