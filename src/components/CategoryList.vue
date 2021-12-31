77931a754a4857b46093b1efe659aa17ad6df28c
Wendy Fu <wendy@wovn.io>
2021-12-31 20:07:01 +0700

    Initialize forum categories

--- src/components/CategoryList.vue
+++ src/components/CategoryList.vue
@@ -1,21 +1,21 @@
 <template>
   <div>
-    <ForumListItem
-      v-for="forum in forums"
-      :forum="forum"
-      :key="forum['.key']"
+    <CategoryListItem
+      v-for="category in categories"
+      :category="category"
+      :key="category['.key']"
     />
   </div>
 </template>
 
 <script>
-    import ForumListItem from './ForumListItem'
+    import CategoryListItem from './CategoryListItem'
     export default {
       components: {
-        ForumListItem
+        CategoryListItem
       },
       props: {
-        forums: {
+        categories: {
           required: true,
           type: Array
         }
--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,23 +1,21 @@
 <template>
   <div class="col-full">
     <h1>Welcome to the Forum</h1>
-    <ForumList :forums="forums"/>
+    <CategoryList :categories="categories"/>
   </div>
 </template>
 
 <script>
 import sourceData from '@/data'
-import ForumList from '@/components/ForumList'
+import CategoryList from '@/components/CategoryList'
 
 export default {
   components: {
-    ForumList
+    CategoryList
   },
   data () {
     return {
-      forums: Object.values(sourceData.forums),
-      posts: sourceData.posts,
-      users: sourceData.users
+      categories: Object.values(sourceData.categories)
     }
   }
 }
